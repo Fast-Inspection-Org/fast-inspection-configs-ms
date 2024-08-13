94e76a580ad1617bc3ece37531ebe0fafc3871d4
Adrian <adrian.suarez.alphen@gmail.com>
2024-08-13 00:01:14 -0400

    Eliminación de los decoradores de los roles

--- src/decoradores/rol.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const IS_PUBLIC_KEY = 'isPublic';
-// Forma de crear un decorados personalizado
-export const Roles = (roles: Array<String>) => SetMetadata("roles", roles);
